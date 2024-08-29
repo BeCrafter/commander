39fffed31a4b5f22c5f7a1e609cdac4dd96a8605
Mark.Wang <kugouming@users.noreply.github.com>
2024-08-29 19:06:14 +0800

    update

--- helper/tools.go
+++ helper/tools.go
@@ -53,59 +53,135 @@ func InArrayStr(str string, arr []string) (inArray bool) {
 	return
 }
 
-func interfaceSliceToSortSlice(slice interface{}) (interface{}, bool) {
-	if reflect.TypeOf(slice).Kind() != reflect.Slice {
-		return nil, false
+func interfaceSliceToSortSlice(slice []interface{}) (interface{}, bool) {
+	if len(slice) == 0 || slice[0] == nil {
+		return slice, true
 	}
 
-	switch v := slice.(type) {
-	case []string:
-		SortSlice(v)
-	case []int:
-		SortSlice(v)
-	case []int8:
-		SortSlice(v)
-	case []int16:
-		SortSlice(v)
-	case []int32:
-		SortSlice(v)
-	case []int64:
-		SortSlice(v)
-	case []uint:
-		SortSlice(v)
-	case []uint8:
-		SortSlice(v)
-	case []uint16:
-		SortSlice(v)
-	case []uint32:
-		SortSlice(v)
-	case []uint64:
-		SortSlice(v)
-	case []float32:
-		SortSlice(v)
-	case []float64:
-		SortSlice(v)
-	case []uintptr:
-		SortSlice(v)
-	case []map[string]interface{}:
-		sSlice := make([]string, 0)
-		for _, v := range v {
-			vb, _ := json.Marshal(SortMapValues(v))
+	llen := len(slice)
+	var ret interface{}
+
+	switch slice[0].(type) {
+	case string:
+		typedSlice := make([]string, llen)
+		for i, v := range slice {
+			typedSlice[i] = v.(string)
+		}
+		SortSlice(typedSlice)
+		ret = typedSlice
+	case int:
+		typedSlice := make([]int, llen)
+		for i, v := range slice {
+			typedSlice[i] = v.(int)
+		}
+		SortSlice(typedSlice)
+		ret = typedSlice
+	case int8:
+		typedSlice := make([]int8, llen)
+		for i, v := range slice {
+			typedSlice[i] = v.(int8)
+		}
+		SortSlice(typedSlice)
+		ret = typedSlice
+	case int16:
+		typedSlice := make([]int16, llen)
+		for i, v := range slice {
+			typedSlice[i] = v.(int16)
+		}
+		SortSlice(typedSlice)
+		ret = typedSlice
+	case int32:
+		typedSlice := make([]int32, llen)
+		for i, v := range slice {
+			typedSlice[i] = v.(int32)
+		}
+		SortSlice(typedSlice)
+		ret = typedSlice
+	case int64:
+		typedSlice := make([]int64, llen)
+		for i, v := range slice {
+			typedSlice[i] = v.(int64)
+		}
+		SortSlice(typedSlice)
+		ret = typedSlice
+	case uint:
+		typedSlice := make([]uint, llen)
+		for i, v := range slice {
+			typedSlice[i] = v.(uint)
+		}
+		SortSlice(typedSlice)
+		ret = typedSlice
+	case uint8:
+		typedSlice := make([]uint8, llen)
+		for i, v := range slice {
+			typedSlice[i] = v.(uint8)
+		}
+		SortSlice(typedSlice)
+		ret = typedSlice
+	case uint16:
+		typedSlice := make([]uint16, llen)
+		for i, v := range slice {
+			typedSlice[i] = v.(uint16)
+		}
+		SortSlice(typedSlice)
+		ret = typedSlice
+	case uint32:
+		typedSlice := make([]uint32, llen)
+		for i, v := range slice {
+			typedSlice[i] = v.(uint32)
+		}
+		SortSlice(typedSlice)
+		ret = typedSlice
+	case uint64:
+		typedSlice := make([]uint64, llen)
+		for i, v := range slice {
+			typedSlice[i] = v.(uint64)
+		}
+		SortSlice(typedSlice)
+		ret = typedSlice
+	case float32:
+		typedSlice := make([]float32, llen)
+		for i, v := range slice {
+			typedSlice[i] = v.(float32)
+		}
+		SortSlice(typedSlice)
+		ret = typedSlice
+	case float64:
+		typedSlice := make([]float64, llen)
+		for i, v := range slice {
+			typedSlice[i] = v.(float64)
+		}
+		SortSlice(typedSlice)
+		ret = typedSlice
+	case uintptr:
+		typedSlice := make([]uintptr, llen)
+		for i, v := range slice {
+			typedSlice[i] = v.(uintptr)
+		}
+		SortSlice(typedSlice)
+		ret = typedSlice
+	case map[string]interface{}:
+		sSlice := make([]string, 0, len(slice))
+		for i, v := range slice {
+			vv := sortMapValues(v.(map[string]interface{}))
+			slice[i] = vv
+
+			vb, _ := json.Marshal(vv)
 			sSlice = append(sSlice, string(vb))
 		}
 		sort.Strings(sSlice)
-		var ret = make([]map[string]interface{}, len(sSlice))
 		for i, s := range sSlice {
-			json.Unmarshal([]byte(s), &ret[i])
+			json.Unmarshal([]byte(s), &slice[i])
 		}
-		slice = ret
+		ret = slice
+	default:
+		ret = slice
 	}
-
-	return slice, true
+	return ret, true
 }
 
-// 对map的值进行递归处理，如果值是数组或切片，则排序
-func SortMapValues(m map[string]interface{}) map[string]interface{} {
+// sortMapValues 对map的值进行递归处理，如果值是数组或切片，则排序（仅接收从json反解析后的map，自定义的map这里会报错）
+func sortMapValues(m map[string]interface{}) map[string]interface{} {
 	result := make(map[string]interface{})
 	for k, v := range m {
 		if v == nil {
@@ -115,11 +191,11 @@ func SortMapValues(m map[string]interface{}) map[string]interface{} {
 
 		switch reflect.TypeOf(v).Kind() {
 		case reflect.Slice:
-			result[k], _ = interfaceSliceToSortSlice(v)
+			result[k], _ = interfaceSliceToSortSlice(v.([]interface{}))
 		case reflect.Map:
 			// 递归处理嵌套的map
 			nestedMap := v.(map[string]interface{})
-			sortedMap := SortMapValues(nestedMap)
+			sortedMap := sortMapValues(nestedMap)
 			result[k] = sortedMap
 		default:
 			// 其他类型，直接赋值
@@ -129,15 +205,24 @@ func SortMapValues(m map[string]interface{}) map[string]interface{} {
 	return result
 }
 
-// 对map的值进行递归处理，如果值是数组或切片，则排序
+// SortMapValuesByBytes 对map的值进行递归处理，如果值是数组或切片，则排序
 func SortMapValuesByBytes(jsonBytes []byte) []byte {
 	var m map[string]interface{}
 	if err := json.Unmarshal(jsonBytes, &m); err != nil {
 		return nil
 	}
 
-	result := SortMapValues(m)
+	result := sortMapValues(m)
 	bytes, _ := json.Marshal(result)
 
 	return bytes
+}
+
+// SortMapValuesByMap 对map的值进行递归处理，如果值是数组或切片，则排序 (针对定义好的map需要重新编码)
+func SortMapValuesByMap(m map[string]interface{}) map[string]interface{} {
+	body, _ := json.Marshal(m)
+	var mm map[string]interface{}
+	json.Unmarshal(body, &mm)
+
+	return sortMapValues(mm)
 }
--- helper/tools_test.go
+++ helper/tools_test.go
@@ -1,6 +1,7 @@
 package helper
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,6 +11,16 @@ func TestSortMapValues(t *testing.T) {
 	type args struct {
 		input map[string]interface{}
 	}
+
+	inStr := `{"code":0,"data":{"list":[{"category_id":0,"detail":["https://static0.xx.com/scm/goods-platform/1700118532205_image.png"],"head_picture":["https://static0.xx.com/scm/goods-platform/1700118525742_image.png"],"id":47582,"product_list":[{"RuleDesc":"","code_num":"SP00100001797","desc":"与大师同行礼盒（赠笔记本）","entity":[{"id":2462,"name":"【自采】与大师同行主题礼盒 新"},{"name":"笔记本套装","id":1298},{"id":2461,"name":"【自采】与大师同行主题礼盒袋 新"}],"extra_info":{},"name":"与大师同行礼盒（赠笔记本）","package_id":1296,"package_info":{"package_id":0,"package_type":0},"product_id":47581,"sale_price_cent":490,"sku_entity":{"1091":[{"id":1298,"name":"笔记本套装"}],"2250":[{"id":2461,"name":"与大师同行主题礼盒袋 新"}],"2251":[{"id":2462,"name":"与大师同行主题礼盒 新"}]},"sku_id":2250,"sku_ids":[1091,2251,2250],"source_type":1,"spec_info":[]}],"video":[]}],"total":1},"stat":1,"traceId":"7131e175-2c77-4a43-a30c-0005357ef435"}`
+	var input map[string]interface{}
+	json.Unmarshal([]byte(inStr), &input)
+
+	outStr := `{"code":0,"data":{"list":[{"category_id":0,"detail":["https://static0.xx.com/scm/goods-platform/1700118532205_image.png"],"head_picture":["https://static0.xx.com/scm/goods-platform/1700118525742_image.png"],"id":47582,"product_list":[{"RuleDesc":"","code_num":"SP00100001797","desc":"与大师同行礼盒（赠笔记本）","entity":[{"id":1298,"name":"笔记本套装"},{"id":2461,"name":"【自采】与大师同行主题礼盒袋 新"},{"id":2462,"name":"【自采】与大师同行主题礼盒 新"}],"extra_info":{},"name":"与大师同行礼盒（赠笔记本）","package_id":1296,"package_info":{"package_id":0,"package_type":0},"product_id":47581,"sale_price_cent":490,"sku_entity":{"1091":[{"id":1298,"name":"笔记本套装"}],"2250":[{"id":2461,"name":"与大师同行主题礼盒袋 新"}],"2251":[{"id":2462,"name":"与大师同行主题礼盒 新"}]},"sku_id":2250,"sku_ids":[1091,2250,2251],"source_type":1,"spec_info":[]}],"video":[]}],"total":1},"stat":1,"traceId":"7131e175-2c77-4a43-a30c-0005357ef435"}
+`
+	var output map[string]interface{}
+	json.Unmarshal([]byte(outStr), &output)
+
 	tests := []struct {
 		name     string
 		args     args
@@ -30,29 +41,29 @@ func TestSortMapValues(t *testing.T) {
 			name: "sort int slice",
 			args: args{
 				input: map[string]interface{}{
-					"key1": []int{3, 1, 2},
+					"key1": []float64{3, 1, 2}, // 此处不能定义为其他类型，受限于json.Unmarshal的限制，只能定义为float64
 				},
 			},
 			expected: map[string]interface{}{
-				"key1": []int{1, 2, 3},
+				"key1": []float64{1, 2, 3},
 			},
 		},
 		{
 			name: "sort map in slice",
 			args: args{
 				input: map[string]interface{}{
-					"key1": []map[string]interface{}{
-						{"sort_key": "c"},
-						{"sort_key": "a"},
-						{"sort_key": "b"},
+					"key1": []interface{}{
+						map[string]interface{}{"sort_key": "c"},
+						map[string]interface{}{"sort_key": "a"},
+						map[string]interface{}{"sort_key": "b"},
 					},
 				},
 			},
 			expected: map[string]interface{}{
-				"key1": []map[string]interface{}{
-					{"sort_key": "a"},
-					{"sort_key": "b"},
-					{"sort_key": "c"},
+				"key1": []interface{}{
+					map[string]interface{}{"sort_key": "a"},
+					map[string]interface{}{"sort_key": "b"},
+					map[string]interface{}{"sort_key": "c"},
 				},
 			},
 		},
@@ -64,7 +75,7 @@ func TestSortMapValues(t *testing.T) {
 						"key11": "a",
 						"key12": []string{"c", "b", "a"},
 						"key13": map[string]interface{}{
-							"key131": []int{2, 3, 1},
+							"key131": []float64{2, 3, 1},
 						},
 					},
 				},
@@ -74,15 +85,22 @@ func TestSortMapValues(t *testing.T) {
 					"key11": "a",
 					"key12": []string{"a", "b", "c"},
 					"key13": map[string]interface{}{
-						"key131": []int{1, 2, 3},
+						"key131": []float64{1, 2, 3},
 					},
 				},
 			},
 		},
+		{
+			name: "Multi level map sorting",
+			args: args{
+				input: input,
+			},
+			expected: output,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			assert.EqualValues(t, tt.expected, SortMapValues(tt.args.input))
+			assert.EqualValues(t, tt.expected, SortMapValuesByMap(tt.args.input))
 		})
 	}
 }
